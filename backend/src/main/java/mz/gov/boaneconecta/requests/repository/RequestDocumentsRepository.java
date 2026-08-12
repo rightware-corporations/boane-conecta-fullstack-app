b1bbbe59f8448ebb6c6b05058a05c2ff75e8d1e0
rightware-corporations <rightware123@gmail.com>
2026-08-12 16:09:40 +0200

    feat: add request document link repository

---
+++ backend/src/main/java/mz/gov/boaneconecta/requests/repository/RequestDocumentsRepository.java
@@ -0,0 +1,15 @@
+package mz.gov.boaneconecta.requests.repository;
+
+import mz.gov.boaneconecta.documents.entity.Document;
+import mz.gov.boaneconecta.requests.entity.CitizenRequest;
+import mz.gov.boaneconecta.requests.entity.RequestDocuments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RequestDocumentsRepository extends JpaRepository<RequestDocuments, RequestDocuments.RequestDocumentId> {
+    List<RequestDocuments> findByRequestOrderByDocumentCreatedAtDesc(CitizenRequest request);
+    boolean existsByRequestAndDocument(CitizenRequest request, Document document);
+}
