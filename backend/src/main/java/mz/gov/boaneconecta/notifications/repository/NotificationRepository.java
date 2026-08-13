1cd05b7db28e2d6b407430f2b2a860be6da956af
rightware-corporations <rightware123@gmail.com>
2026-08-13 14:55:31 +0200

    feat: add notification repository

---
+++ backend/src/main/java/mz/gov/boaneconecta/notifications/repository/NotificationRepository.java
@@ -0,0 +1,17 @@
+package mz.gov.boaneconecta.notifications.repository;
+
+import mz.gov.boaneconecta.notifications.entity.Notification;
+import mz.gov.boaneconecta.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+
+    Optional<Notification> findByIdAndUser(UUID id, User user);
+
+    long countByUserAndReadAtIsNull(User user);
+}
