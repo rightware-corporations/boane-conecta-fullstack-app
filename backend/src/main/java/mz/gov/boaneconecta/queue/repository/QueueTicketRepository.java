bb25490594a5dd72a161fd708b8453610395fd8c
rightware-corporations <rightware123@gmail.com>
2026-08-28 17:57:09 +0200

    fix(backend): resolve f5 domain compilation imports

--- backend/src/main/java/mz/gov/boaneconecta/queue/repository/QueueTicketRepository.java
+++ backend/src/main/java/mz/gov/boaneconecta/queue/repository/QueueTicketRepository.java
@@ -4,6 +4,7 @@ import mz.gov.boaneconecta.appointments.entity.Appointment;
 import mz.gov.boaneconecta.departments.entity.Department;
 import mz.gov.boaneconecta.queue.entity.QueueTicket;
 import mz.gov.boaneconecta.queue.entity.QueueTicketStatus;
+import mz.gov.boaneconecta.queue.entity.MunicipalQueue;
 import mz.gov.boaneconecta.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
