2f8f989561096f12c167667f66b09fd15cc6f287
rightware-corporations <rightware123@gmail.com>
2026-08-28 17:57:04 +0200

    fix(backend): resolve f5 domain compilation imports

--- backend/src/main/java/mz/gov/boaneconecta/appointments/repository/AppointmentSlotRepository.java
+++ backend/src/main/java/mz/gov/boaneconecta/appointments/repository/AppointmentSlotRepository.java
@@ -1,6 +1,7 @@
 package mz.gov.boaneconecta.appointments.repository;
 
 import mz.gov.boaneconecta.appointments.entity.AppointmentSlot;
+import mz.gov.boaneconecta.appointments.entity.AppointmentScheduleRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Lock;
